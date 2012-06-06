5c6d553525b10d8bd04cf772b71c748e354f7427
Daniel <dbr@cpan.org>
2012-06-06 07:22:29 +0000

    Import of DBR/Task-BeLike-DBR-0.022 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-DBR
    gitpan-cpan-version:      0.022
    gitpan-cpan-path:         DBR/Task-BeLike-DBR-0.022.tar.gz
    gitpan-cpan-author:       DBR
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/DBR.pm
+++ lib/Task/BeLike/DBR.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Task::BeLike::DBR::AUTHORITY = 'cpan:DBR';
 }
 {
-  $Task::BeLike::DBR::VERSION = '0.021';
+  $Task::BeLike::DBR::VERSION = '0.022';
 }
 
 # ABSTRACT: Modules that DBR likes to use.
@@ -22,7 +22,7 @@ Task::BeLike::DBR - Modules that DBR likes to use.
 
 =head1 VERSION
 
-version 0.021
+version 0.022
 
 =head1 AUTHOR
 
